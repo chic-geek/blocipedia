9144c08f6437ebe98e86a265590462f9dd3f71ff
Chris Brett <c.brett@icloud.com>
2015-02-21 10:43:11 +0000

    Add in some routing comments to help with understanding tests

--- spec/features/wiki/edit_spec.rb
+++ spec/features/wiki/edit_spec.rb
@@ -16,19 +16,19 @@ feature 'Wiki edit/update', type: :feature do
     fill_in "Password", with: "test-password"
     click_button "Log in"
 
-    #
+    # localhost:3000/wikis
     expect(page).to have_content(wiki.title)
     expect(current_path).to eq(wikis_path)
     find('a[title="edit-item"]').click
 
-    #
+    # localhost:3000/wikis/1/edit
     expect(page).to have_css("h1", "Edit wiki")
     expect(current_path).to eq(edit_wiki_path(wiki))
     fill_in "Title", with: "Dogs"
     fill_in "Body", with: "Dogs are a wonderful pet, they require lots of attention and exercise!"
     click_button "Update"
 
-    #
+    # localhost:3000/wikis/1
     expect(page).to have_content("Dogs")
     expect(page).to have_content("Dogs are a wonderful pet, they require lots of attention and exercise!")
 
